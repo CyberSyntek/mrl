e8e68364aff16b278fd364d721106d75ecd5a0d0
CyberSyntek <81597534+CyberSyntek@users.noreply.github.com>
2024-03-19 06:49:49 -0400

    Add files via upload

---
+++ Custom/Gestures/evilLaugh1.py
@@ -0,0 +1,434 @@
+def evilLaugh1():
+
+    #SET SPEED
+    i01_leftArm_omoplate.setMaxSpeed()
+    i01_rightArm_omoplate.setMaxSpeed()
+    i01_leftArm_shoulder.setMaxSpeed()
+    i01_rightArm_shoulder.setMaxSpeed()
+    i01_leftArm_rotate.setMaxSpeed()
+    i01_rightArm_rotate.setMaxSpeed()
+    i01_leftArm_bicep.setMaxSpeed()
+    i01_rightArm_bicep.setMaxSpeed()
+    i01_leftHand_thumb.setMaxSpeed()
+    i01_leftHand_index.setMaxSpeed()
+    i01_leftHand_majeure.setMaxSpeed()
+    i01_leftHand_ringFinger.setMaxSpeed()
+    i01_leftHand_pinky.setMaxSpeed()
+    i01_leftHand_wrist.setMaxSpeed()
+    i01_rightHand_thumb.setMaxSpeed()
+    i01_rightHand_index.setMaxSpeed()
+    i01_rightHand_majeure.setMaxSpeed()
+    i01_rightHand_ringFinger.setMaxSpeed()
+    i01_rightHand_pinky.setMaxSpeed()
+    i01_rightHand_wrist.setMaxSpeed()
+
+    i01_mouth.audioFile.play('C:\MRL\Custom\Audio\AluLaugh01.mp3')
+
+    #EYELIDS
+    i01_head_eyelidLeftUpper.moveTo(0)
+    i01_head_eyelidLeftLower.moveTo(0)
+    i01_head_eyelidRightUpper.moveTo(0)
+    i01_head_eyelidRightLower.moveTo(0)
+
+    #EYEBROWS
+    i01_head_eyebrowLeft.moveTo(180)
+    i01_head_forheadLeft.moveTo(90)
+    i01_head_eyebrowRight.moveTo(180)
+    i01_head_forheadRight.moveTo(90)
+
+    #Shoulder slight raise
+    i01_leftArm_shoulder.moveTo(40)
+    i01_rightArm_shoulder.moveTo(40)
+
+    #UP
+    i01_head_neck.moveTo(140)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.1)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.1)
+
+    i01_head_neck.moveTo(140)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.1)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.1)
+
+    i01_head_neck.moveTo(140)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.1)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(160)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(15)
+    i01_rightArm_omoplate.moveTo(15)
+    sleep(0.25)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(0)
+    i01_rightArm_omoplate.moveTo(0)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    #STOMACH
+    i01_torso_topStom.setSpeed(84)
+    i01_torso_midStom.setSpeed(132)
+    i01_torso_topStom.moveTo(60)
+    i01_torso_midStom.moveTo(120)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(180)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(15)
+    i01_rightArm_omoplate.moveTo(15)
+    sleep(0.25)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(0)
+    i01_rightArm_omoplate.moveTo(0)
+    sleep(0.2)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.2)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.2)
+
+    i01_head_neck.moveTo(160)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(15)
+    i01_rightArm_omoplate.moveTo(15)
+    sleep(0.2)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(0)
+    i01_rightArm_omoplate.moveTo(0)
+    sleep(0.2)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.2)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.2)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.2)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.2)
+
+    #STOMACH
+    i01_torso_topStom.moveTo(140)
+    i01_torso_midStom.moveTo(60)
+
+    #######################
+
+    #SHOULDER
+    i01_leftArm_shoulder.moveTo(75)
+    i01_rightArm_shoulder.moveTo(75)
+    sleep(0.5)
+
+    #BICEP
+    i01_leftArm_bicep.moveTo(110)
+    i01_rightArm_bicep.moveTo(100)
+    #ROTATE
+    i01_leftArm_rotate.moveTo(115)
+    i01_rightArm_rotate.moveTo(115)
+    sleep(0.7)
+
+    #WRIST
+    i01_rightHand_wrist.moveTo(30)
+    i01_leftHand_wrist.moveTo(150)
+    sleep(0.1)
+
+    #######################
+
+    i01_head_neck.moveTo(180)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(15)
+    i01_rightArm_omoplate.moveTo(15)
+
+    #SEMI CLOSED FINGERS
+    i01_leftHand_thumb.moveTo(150)
+    i01_leftHand_index.moveTo(120)
+    i01_leftHand_majeure.moveTo(100)
+    i01_leftHand_ringFinger.moveTo(100)
+    i01_leftHand_pinky.moveTo(120)
+    i01_rightHand_thumb.moveTo(130)
+    i01_rightHand_index.moveTo(160)
+    i01_rightHand_majeure.moveTo(120)
+    i01_rightHand_ringFinger.moveTo(120)
+    i01_rightHand_pinky.moveTo(120)
+    sleep(0.12)
+
+    i01_head_neck.moveTo(140)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(0)
+    i01_rightArm_omoplate.moveTo(0)
+
+    #FINGERS OPEN
+    i01_leftHand_thumb.moveTo(0)
+    i01_leftHand_index.moveTo(20)
+    i01_leftHand_majeure.moveTo(20)
+    i01_leftHand_ringFinger.moveTo(20)
+    i01_leftHand_pinky.moveTo(20)
+    i01_rightHand_thumb.moveTo(20)
+    i01_rightHand_index.moveTo(30)
+    i01_rightHand_majeure.moveTo(20)
+    i01_rightHand_ringFinger.moveTo(20)
+    i01_rightHand_pinky.moveTo(20)
+    sleep(0.12)
+
+    i01_head_neck.moveTo(160)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(15)
+    i01_rightArm_omoplate.moveTo(15)
+
+    #SEMI CLOSED FINGERS
+    i01_leftHand_thumb.moveTo(150)
+    i01_leftHand_index.moveTo(120)
+    i01_leftHand_majeure.moveTo(100)
+    i01_leftHand_ringFinger.moveTo(100)
+    i01_leftHand_pinky.moveTo(120)
+    i01_rightHand_thumb.moveTo(130)
+    i01_rightHand_index.moveTo(160)
+    i01_rightHand_majeure.moveTo(120)
+    i01_rightHand_ringFinger.moveTo(120)
+    i01_rightHand_pinky.moveTo(120)
+    sleep(0.12)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(0)
+    i01_rightArm_omoplate.moveTo(0)
+
+    #FINGERS OPEN
+    i01_leftHand_thumb.moveTo(0)
+    i01_leftHand_index.moveTo(20)
+    i01_leftHand_majeure.moveTo(20)
+    i01_leftHand_ringFinger.moveTo(20)
+    i01_leftHand_pinky.moveTo(20)
+    i01_rightHand_thumb.moveTo(20)
+    i01_rightHand_index.moveTo(30)
+    i01_rightHand_majeure.moveTo(20)
+    i01_rightHand_ringFinger.moveTo(20)
+    i01_rightHand_pinky.moveTo(20)
+    sleep(0.12)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    #SEMI CLOSED FINGERS
+    i01_leftHand_thumb.moveTo(150)
+    i01_leftHand_index.moveTo(120)
+    i01_leftHand_majeure.moveTo(100)
+    i01_leftHand_ringFinger.moveTo(100)
+    i01_leftHand_pinky.moveTo(120)
+    i01_rightHand_thumb.moveTo(130)
+    i01_rightHand_index.moveTo(160)
+    i01_rightHand_majeure.moveTo(120)
+    i01_rightHand_ringFinger.moveTo(120)
+    i01_rightHand_pinky.moveTo(120)
+    sleep(0.12)
+
+    i01_head_neck.moveTo(130)
+    i01_head_rollNeck.moveTo(90)
+    #FINGERS OPEN
+    i01_leftHand_thumb.moveTo(0)
+    i01_leftHand_index.moveTo(20)
+    i01_leftHand_majeure.moveTo(20)
+    i01_leftHand_ringFinger.moveTo(20)
+    i01_leftHand_pinky.moveTo(20)
+    i01_rightHand_thumb.moveTo(20)
+    i01_rightHand_index.moveTo(30)
+    i01_rightHand_majeure.moveTo(20)
+    i01_rightHand_ringFinger.moveTo(20)
+    i01_rightHand_pinky.moveTo(20)
+    sleep(0.12)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(0)
+    i01_rightArm_omoplate.moveTo(0)
+    #SEMI CLOSED FINGERS
+    i01_leftHand_thumb.moveTo(150)
+    i01_leftHand_index.moveTo(120)
+    i01_leftHand_majeure.moveTo(100)
+    i01_leftHand_ringFinger.moveTo(100)
+    i01_leftHand_pinky.moveTo(120)
+    i01_rightHand_thumb.moveTo(130)
+    i01_rightHand_index.moveTo(160)
+    i01_rightHand_majeure.moveTo(120)
+    i01_rightHand_ringFinger.moveTo(120)
+    i01_rightHand_pinky.moveTo(120)
+    sleep(0.12)
+
+    i01_head_neck.moveTo(180)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(15)
+    i01_rightArm_omoplate.moveTo(15)
+    #FINGERS OPEN
+    i01_leftHand_thumb.moveTo(0)
+    i01_leftHand_index.moveTo(20)
+    i01_leftHand_majeure.moveTo(20)
+    i01_leftHand_ringFinger.moveTo(20)
+    i01_leftHand_pinky.moveTo(20)
+    i01_rightHand_thumb.moveTo(20)
+    i01_rightHand_index.moveTo(30)
+    i01_rightHand_majeure.moveTo(20)
+    i01_rightHand_ringFinger.moveTo(20)
+    i01_rightHand_pinky.moveTo(20)
+    sleep(0.12)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(0)
+    i01_rightArm_omoplate.moveTo(0)
+    #SEMI CLOSED FINGERS
+    i01_leftHand_thumb.moveTo(150)
+    i01_leftHand_index.moveTo(120)
+    i01_leftHand_majeure.moveTo(100)
+    i01_leftHand_ringFinger.moveTo(100)
+    i01_leftHand_pinky.moveTo(120)
+    i01_rightHand_thumb.moveTo(130)
+    i01_rightHand_index.moveTo(160)
+    i01_rightHand_majeure.moveTo(120)
+    i01_rightHand_ringFinger.moveTo(120)
+    i01_rightHand_pinky.moveTo(120)
+    sleep(0.12)
+
+    #STOMACH
+    i01_torso_topStom.moveTo(90)
+    i01_torso_midStom.moveTo(90)
+
+    i01_head_neck.moveTo(180)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(15)
+    i01_rightArm_omoplate.moveTo(15)
+    #FINGERS OPEN
+    i01_leftHand_thumb.moveTo(0)
+    i01_leftHand_index.moveTo(20)
+    i01_leftHand_majeure.moveTo(20)
+    i01_leftHand_ringFinger.moveTo(20)
+    i01_leftHand_pinky.moveTo(20)
+    i01_rightHand_thumb.moveTo(20)
+    i01_rightHand_index.moveTo(30)
+    i01_rightHand_majeure.moveTo(20)
+    i01_rightHand_ringFinger.moveTo(20)
+    i01_rightHand_pinky.moveTo(20)
+    sleep(0.25)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(0)
+    i01_rightArm_omoplate.moveTo(0)
+    sleep(0.15)
+
+    i01_head_neck.moveTo(180)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(10)
+    i01_rightArm_omoplate.moveTo(10)
+    sleep(0.25)
+
+    i01_head_neck.moveTo(150)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(0)
+    i01_rightArm_omoplate.moveTo(0)
+    sleep(0.26)
+
+    i01_head_neck.moveTo(180)
+    i01_head_rollNeck.moveTo(90)
+    i01_leftArm_omoplate.moveTo(10)
+    i01_rightArm_omoplate.moveTo(10)
+    sleep(0.15)
+
+    #ARM AND NECK RESET/REST
+    i01_leftArm_bicep.moveTo(5)
+    i01_leftArm_rotate.moveTo(90)
+    i01_leftArm_shoulder.moveTo(30)
+    i01_leftArm_omoplate.moveTo(5)
+    i01_rightArm_bicep.moveTo(5)
+    i01_rightArm_rotate.moveTo(90)
+    i01_rightArm_shoulder.moveTo(30)
+    i01_rightArm_omoplate.moveTo(5)
+    i01_leftHand_thumb.moveTo(0)
+    i01_leftHand_index.moveTo(0)
+    i01_leftHand_majeure.moveTo(0)
+    i01_leftHand_ringFinger.moveTo(0)
+    i01_leftHand_pinky.moveTo(0)
+    i01_leftHand_wrist.moveTo(0)
+    i01_rightHand_thumb.moveTo(0)
+    i01_rightHand_index.moveTo(0)
+    i01_rightHand_majeure.moveTo(0)
+    i01_rightHand_ringFinger.moveTo(0)
+    i01_rightHand_pinky.moveTo(0)
+    i01_rightHand_wrist.moveTo(180)
+    i01_head_neck.moveTo(90)
+    i01_head_rollNeck.moveTo(90)
+    sleep(0.7)
+
+    #EYELID AND BROWS RESET/REST
+    i01_head_eyelidLeftUpper.moveTo(90)
+    i01_head_eyelidLeftLower.moveTo(90)
+    i01_head_eyelidRightUpper.moveTo(90)
+    i01_head_eyelidRightLower.moveTo(90)
+    i01_head_eyebrowLeft.moveTo(90)
+    i01_head_forheadLeft.moveTo(90)
+    i01_head_eyebrowRight.moveTo(90)
+    i01_head_forheadRight.moveTo(90)
+
+    i01_torso_topStom.setMaxSpeed()
+    i01_torso_midStom.setMaxSpeed()
